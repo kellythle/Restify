8e209849b5539696a14ac7d3e46df60935403bda
Kelly Le <lekelly@teach.cs.utoronto.ca>
2023-03-18 17:58:48 -0400

    deleted imports

--- P2/restify/properties/views/deletereservation.py
+++ P2/restify/properties/views/deletereservation.py
@@ -5,18 +5,6 @@ from rest_framework.response import Response
 from rest_framework.generics import DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.urls import reverse_lazy
-from properties.forms import AddPropertyForm, EditPropertyForm, PropertyImageForm
-from properties.models import Property, PropertyImages
-from properties.serializers import PropertySerializer, PropertyImageSerializer
-from datetime import datetime
-from rest_framework.response import Response
-from rest_framework.generics import DestroyAPIView
-
 
 class DeleteReservation(DestroyAPIView):
     serializer = ReservationSerializer
