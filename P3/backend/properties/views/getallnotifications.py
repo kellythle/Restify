c70396d6781bb020a3c1c2a88de029ce5b6a105f
Kelly Le <lekelly@teach.cs.utoronto.ca>
2023-04-23 15:58:12 -0400

    pagination

--- P3/backend/properties/views/getallnotifications.py
+++ P3/backend/properties/views/getallnotifications.py
@@ -15,7 +15,7 @@ from rest_framework.views import APIView
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
